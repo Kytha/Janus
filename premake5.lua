8702559d2086d6b22e999f8e3cca5be63062c52b
Kytha <thatchek@mcmaster.ca>
2021-03-11 02:28:09 -0500

    General cleanup of repo

--- premake5.lua
+++ premake5.lua
@@ -17,10 +17,11 @@ IncludeDir["GLFW"] = "Janus/vendor/GLFW/include"
 IncludeDir["Glad"] = "Janus/vendor/Glad/include"
 IncludeDir["ImGui"] = "Janus/vendor/ImGui"
 IncludeDir["glm"] = "Janus/vendor/glm"
-
-include "Janus/vendor/GLFW"
-include "Janus/vendor/Glad"
-include "Janus/vendor/ImGui"
+group "Dependencies"
+	include "Janus/vendor/GLFW"
+	include "Janus/vendor/Glad"
+	include "Janus/vendor/ImGui"
+group ""
 
 project "Janus"
 	location "Janus"
@@ -141,3 +142,54 @@ project "Sandbox"
 		defines "JN_DIST"
 		runtime "Release"
 		optimize "on"
+
+		project "Sandbox"
+		location "Sandbox"
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++17"
+		staticruntime "on"
+	
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp"
+		}
+	
+		includedirs
+		{
+			"Janus/vendor/spdlog/include",
+			"Janus/src",
+			"%{IncludeDir.glm}",
+			"Janus/vendor"
+		}
+	
+		links {
+			"Janus"
+		}
+	
+		filter "system:windows"
+			systemversion "latest"
+	
+			defines
+			{
+				"JN_PLATFORM_WINDOWS"
+			}
+	
+		filter "configurations:Debug"
+			defines "JN_DEBUG"
+			runtime "Debug"
+			symbols "on"
+	
+		filter "configurations:Release"
+			defines "JN_RELEASE"
+			runtime "Release"
+			optimize "on"
+	
+		filter "configurations:Dist"
+			defines "JN_DIST"
+			runtime "Release"
+			optimize "on"
